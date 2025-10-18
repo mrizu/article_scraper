e8344aa28f033882554e0086cb313e54b12439ce
Karol Draszawka <60969940+mrizu@users.noreply.github.com>
2025-10-18 19:15:04 +0200

    Add publish time scraper and updated article-content scraper

--- scraper/article_scraper/scraper/scraper.py
+++ scraper/article_scraper/scraper/scraper.py
@@ -3,24 +3,27 @@ from bs4 import BeautifulSoup
 from playwright.sync_api import sync_playwright
 
 
-def get_html(url):
+def get_html(url, headers):
     response = requests.get(url)
     response.raise_for_status()
     soup = BeautifulSoup(response.text, "lxml")
     print(soup.prettify())
-    # metatags = soup.find_all("meta")
-    # for tag in metatags:
-    #     print(tag)
+    metatags = soup.find_all("meta")
+    for tag in metatags:
+        print(tag)
 
 
-def scrape_title(url):
-    response = requests.get(url)
+def scrape_title(url, headers):
+    response = requests.get(url, headers=headers)
     response.raise_for_status()
     soup = BeautifulSoup(response.text, "lxml")
 
     title = soup.find("title")
 
-    return title.get_text(strip=True)
+    if title:
+        return title
+    else:
+        return "Title not found"
 
 
 def scrape_article_content(url):
@@ -29,19 +32,31 @@ def scrape_article_content(url):
         page = browser.new_page()
         page.goto(url)
 
-        page.wait_for_selector("div.article-content")
+        if page.is_visible("div.article-content"):
+            page.wait_for_selector("div.article-content")
+            html = page.content()
+            soup = BeautifulSoup(html, "html.parser")
 
-        html = page.content()
-        soup = BeautifulSoup(html, "html.parser")
-
-        article_div = soup.find("div", class_="article-content")
-        if article_div:
-            return article_div.get_text(separator="\n", strip=True)
+            article_div = soup.find("div", class_="article-content")
         else:
-            return "Article not found"
+            html = page.content()
+            soup = BeautifulSoup(html, "html.parser")
+
+            article_div = soup.find(
+                "div", class_="post-text-two-red table-post mt-8 quote-red link-red"
+            )
+
+        if article_div:
+            return article_div
+        else:
+            return "Article content not found"
 
 
-def scrape_publish_time(url):
-    response = requests.get(url)
+def scrape_publish_time(url, headers):
+    response = requests.get(url, headers=headers)
     response.raise_for_status()
     soup = BeautifulSoup(response.text, "lxml")
+
+    published_time = soup.find("meta", attrs={"property": "article:published_time"})
+
+    return published_time
--- scraper/article_scraper/scraper/views.py
+++ scraper/article_scraper/scraper/views.py
@@ -1,19 +1,34 @@
 from django.shortcuts import render, HttpResponse
 from django.http import JsonResponse
-from .scraper import scrape_title, scrape_article_content, get_html
+from .scraper import scrape_title, scrape_article_content, get_html, scrape_publish_time
 
 
 # Create your views here.
 def home(request):
-    url = "https://take-group.github.io/example-blog-without-ssr/jak-kroic-piers-z-kurczaka-aby-uniknac-suchych-kawalkow-miesa"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+    }
+    # url = "https://take-group.github.io/example-blog-without-ssr/jak-kroic-piers-z-kurczaka-aby-uniknac-suchych-kawalkow-miesa"
+    # url = "https://take-group.github.io/example-blog-without-ssr/co-mozna-zrobic-ze-schabu-oprocz-kotletow-5-zaskakujacych-przepisow"
+    url = "https://galicjaexpress.pl/ford-c-max-jaki-silnik-benzynowy-wybrac-aby-zaoszczedzic-na-paliwie"
     try:
-        # title = scrape_title(url)
+        # title = scrape_title(url, headers).get_text(strip=True)
         # return JsonResponse({"title": title}, json_dumps_params={"ensure_ascii": False})
-        article_content = scrape_article_content(url)
-        get_html(url)
+        # article_content_html = str(scrape_article_content(url))
+        article_content = scrape_article_content(url).get_text(
+            separator="\n", strip=True
+        )
         return JsonResponse(
             {"article-content": article_content},
             json_dumps_params={"ensure_ascii": False},
         )
+        # published_time = scrape_publish_time(url, headers).get("content")
+        # get_html(url)
+        # return JsonResponse(
+        #     {"published_at": published_time},
+        #     json_dumps_params={"ensure_ascii": False},
+        # )
     except Exception as e:
         return JsonResponse({"error": str(e)}, status=500)
